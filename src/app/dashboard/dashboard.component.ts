a9f69cdc0be2c8cc21575a4687d88d6be1a4a164
kev4142 <arkaine04@gmail.com>
2025-01-20 19:33:18 -0600

    Dashboad, Header, NoFound

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule],
+  imports: [CommonModule,HeaderComponent],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,48 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styles: ``
+})
+export class HeaderComponent implements OnInit {
+  userName: string | undefined;
+  userType: string | undefined;
+
+  constructor(private authService: AuthService,private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadUserDetails();
+  }
+  loadUserDetails(): void {
+    this.authService.getUserDetails().subscribe({
+      next: (response) => {
+        this.userName = response.nombreCompleto;
+        this.userType = response.tipo;
+      },
+      error: (erro) => {
+        console.error('Error al Obtener Perfil de Usuario.', erro);
+        this.router.navigate(['/login']);
+      }
+  });
+  }
+  isAdmin(): boolean {
+    return this.authService.isAdministrador();
+  }
+
+  isOperator(): boolean {
+    return this.userType === 'Operador';
+  }
+  optTablero():void{
+    this.router.navigate(['/dashboard'])
+  }
+  salir(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+}
